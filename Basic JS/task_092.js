734bae13e816f3a1aaa5dfb1ac185a3f2414954b
Andrey <andrey.72006@gmail.com>
2022-03-02 22:09:07 +0300

    Manipulating Complex Objects

---
+++ Basic JS/task_092.js
@@ -0,0 +1,25 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Red",
+    "title": "No name",
+    "release_year": 1981,
+    "formats": [
+      "CD",
+      "8T",
+      "LP",
+      "MP3"
+    ],
+    "silver": true
+  }
+];
