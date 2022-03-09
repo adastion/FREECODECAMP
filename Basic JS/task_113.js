75d50a646610d7da6215f433971438c0d7398c0e
Andrey <andrey.72006@gmail.com>
2022-03-09 21:27:45 +0300

    Use Recursion to Create a Range of Numbers

---
+++ Basic JS/task_113.js
@@ -0,0 +1,6 @@
+function rangeOfNumbers(startNum, endNum) {
+  return startNum === endNum
+    ? [startNum]
+    : rangeOfNumbers(startNum, endNum - 1).concat(endNum);
+};
+console.log(rangeOfNumbers(1, 15));
